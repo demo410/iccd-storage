2253bb6f5f0a0f48bb2669996675ef09c8c209d8
shike <shike_ge@163.com>
2023-08-14 10:42:33 +0800

    更新配置文件

--- server/src/main/java/com/iccd/storage/server/modules/share/controller/ShareController.java
+++ server/src/main/java/com/iccd/storage/server/modules/share/controller/ShareController.java
@@ -15,6 +15,7 @@ import com.iccd.storage.server.modules.share.convert.ShareConverter;
 import com.iccd.storage.server.modules.share.po.CancelSharePO;
 import com.iccd.storage.server.modules.share.po.CheckShareCodePO;
 import com.iccd.storage.server.modules.share.po.CreateShareUrlPO;
+import com.iccd.storage.server.modules.share.po.ShareSavePO;
 import com.iccd.storage.server.modules.share.service.IShareService;
 import com.iccd.storage.server.modules.share.vo.ShareDetailVO;
 import com.iccd.storage.server.modules.share.vo.ShareSimpleDetailVO;
@@ -27,6 +28,7 @@ import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -167,47 +169,47 @@ public class ShareController {
         List<UserFileVO> result = iShareService.fileList(context);
         return R.data(result);
     }
-//
-//    @ApiOperation(
-//            value = "保存至我的网盘",
-//            notes = "该接口提供了保存至我的网盘的功能",
-//            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-//            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
-//    )
-//    @NeedShareCode
-//    @PostMapping("share/save")
-//    public R saveFiles(@Validated @RequestBody ShareSavePO shareSavePO) {
-//        ShareSaveContext context = new ShareSaveContext();
-//
-//        String fileIds = shareSavePO.getFileIds();
-//        List<Long> fileIdList = Splitter.on(RPanConstants.COMMON_SEPARATOR).splitToList(fileIds).stream().map(IdUtil::decrypt).collect(Collectors.toList());
-//        context.setFileIdList(fileIdList);
-//
-//        context.setTargetParentId(IdUtil.decrypt(shareSavePO.getTargetParentId()));
-//        context.setUserId(UserIdUtil.get());
-//        context.setShareId(ShareIdUtil.get());
-//
-//        iShareService.saveFiles(context);
-//        return R.success();
-//    }
-//
-//    @ApiOperation(
-//            value = "分享文件下载",
-//            notes = "该接口提供了分享文件下载的功能",
-//            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-//            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
-//    )
-//    @GetMapping("share/file/download")
-//    @NeedShareCode
-//    public void download(@NotBlank(message = "文件ID不能为空") @RequestParam(value = "fileId", required = false) String fileId,
-//                         HttpServletResponse response) {
-//        ShareFileDownloadContext context = new ShareFileDownloadContext();
-//        context.setFileId(IdUtil.decrypt(fileId));
-//        context.setShareId(ShareIdUtil.get());
-//        context.setUserId(UserIdUtil.get());
-//        context.setResponse(response);
-//        iShareService.download(context);
-//    }
+
+    @ApiOperation(
+            value = "保存至我的网盘",
+            notes = "该接口提供了保存至我的网盘的功能",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    @NeedShareCode
+    @PostMapping("share/save")
+    public R saveFiles(@Validated @RequestBody ShareSavePO shareSavePO) {
+        ShareSaveContext context = new ShareSaveContext();
+
+        String fileIds = shareSavePO.getFileIds();
+        List<Long> fileIdList = Splitter.on(StorageConstants.COMMON_SEPARATOR).splitToList(fileIds).stream().map(IdUtil::decrypt).collect(Collectors.toList());
+        context.setFileIdList(fileIdList);
+
+        context.setTargetParentId(IdUtil.decrypt(shareSavePO.getTargetParentId()));
+        context.setUserId(UserIdUtil.get());
+        context.setShareId(ShareIdUtil.get());
+
+        iShareService.saveFiles(context);
+        return R.success();
+    }
+
+    @ApiOperation(
+            value = "分享文件下载",
+            notes = "该接口提供了分享文件下载的功能",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    @GetMapping("share/file/download")
+    @NeedShareCode
+    public void download(@NotBlank(message = "文件ID不能为空") @RequestParam(value = "fileId", required = false) String fileId,
+                         HttpServletResponse response) {
+        ShareFileDownloadContext context = new ShareFileDownloadContext();
+        context.setFileId(IdUtil.decrypt(fileId));
+        context.setShareId(ShareIdUtil.get());
+        context.setUserId(UserIdUtil.get());
+        context.setResponse(response);
+        iShareService.download(context);
+    }
 
 }
 
